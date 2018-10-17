7405f4ee9f1edc50714d68d99c27e4d7c66c0dd7
kamil <kamil.bizon@protonmail.com>
2018-10-17 20:18:01 +0200

    Add simple response to channel from bot

--- bot.js
+++ bot.js
@@ -12,3 +12,13 @@ client.on('ready', () => {
   console.log('Info: connected');
   console.log(`Logged as: ${client.user.username} - ${client.user.id}`);
 });
+
+client.on ('message', (message) => {
+  let msg = message.content;
+  if(msg.substring(0, 1) === prefix) {
+    let args = msg.substring(1).split(' ');
+    // First word after prefix determines currency for which user wants to know price 
+    let command = args[0];
+    message.channel.send('It works')
+  }
+});
